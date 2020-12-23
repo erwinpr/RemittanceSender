6533bd617c456acd7f389ed1926c7286f3cecd95
ERWIN PRASETYO <19031735@AD.BANKMEGA.LOCAL>
2020-12-23 11:20:54 +0700

    Initial Commit For Remittance Sender

---
+++ frmMain.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TCPSender
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.chkReply = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect To :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Host Address :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Message To Send :";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(119, 45);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(133, 20);
+            this.txtHost.TabIndex = 4;
+            this.txtHost.Text = "10.110.1.100";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(119, 76);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(63, 20);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "28750";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(118, 138);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(670, 20);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(118, 103);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(199, 103);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 8;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(118, 164);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(134, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 318);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 13);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "System Status";
+            // 
+            // chkReply
+            // 
+            this.chkReply.AutoSize = true;
+            this.chkReply.Checked = true;
+            this.chkReply.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkReply.Location = new System.Drawing.Point(307, 169);
+            this.chkReply.Name = "chkReply";
+            this.chkReply.Size = new System.Drawing.Size(156, 17);
+            this.chkReply.TabIndex = 11;
+            this.chkReply.Text = "Wait For Reply From Server";
+            this.chkReply.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkReply);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Remittance Sender - MainMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.CheckBox chkReply;
+    }
+}
+
